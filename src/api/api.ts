dad8762d0a366fdebfd3969986f9fee78d68e12b
ashishkunthe <ashishkunthe2003@gmail.com>
2025-11-01 14:30:43 +0530

    added new changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.83.173:4000/api", // Android emulator
-  // or use your LAN IP if testing on real device e.g. http://192.168.x.x:4000/api
+  baseURL: "http://192.168.83.173:4000/api", //  use your LAN IP if testing on real device
 });
 
 api.interceptors.request.use(async (config) => {
